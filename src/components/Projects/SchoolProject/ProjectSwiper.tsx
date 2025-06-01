03f630fc8fab8f952d274f42fbb957b4e7525d80
bedirgcmz <bdrgcmz@gmail.com>
2025-06-01 12:50:45 +0200

    Added educations json data and changed projects data name

--- src/components/Projects/SchoolProject/ProjectSwiper.tsx
+++ src/components/Projects/SchoolProject/ProjectSwiper.tsx
@@ -1,63 +1,4 @@
 
-
-// 'use client';
-
-// import { useEffect, useState } from 'react';
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import { Navigation, Pagination, A11y } from 'swiper/modules';
-// import ProjectCard from './ProjectCard/ProjectCard';
-
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-
-
-
-// export default function ProjectSwiper() {
-//   const [projects, setProjects] = useState<Project[] | null>(null);
-
-//   useEffect(() => {
-//     fetch('/data/ProjectsData.json')
-//       .then((res) => res.json())
-//       .then((data) => {
-//         console.log('Fetched data:', data);
-//         setProjects(data);
-//       })
-//       .catch((err) => console.error('Error loading projects:', err));
-//   }, []);
-
-//   if (projects === null) {
-//     return <div className="text-center text-gray-400 dark:text-gray-500 py-8">Loading projects...</div>;
-//   }
-
-//   return (
-//     <div className="w-full px-4 py-8 max-w-6xl mx-auto">
-//       <Swiper
-//         modules={[Navigation, Pagination, A11y]}
-//         spaceBetween={24}
-//         slidesPerView={1}
-//         navigation
-//         pagination={{ clickable: true }}
-//         breakpoints={{
-//           640: { slidesPerView: 1 },
-//           768: { slidesPerView: 2 },
-//           1024: { slidesPerView: 3 },
-//         }}
-//         className="rounded-lg"
-//       >
-//         {projects.map((project, index) => (
-//           <SwiperSlide key={index}>
-//             {/* <ProjectCard {...project} /> */}
-//             <div className="h-64 flex text-center text-5xl text-gray-400 dark:text-gray-500 py-8">
-//               Gel Bakalaim artik
-//             </div>
-//           </SwiperSlide>
-//         ))}
-//       </Swiper>
-//     </div>
-//   );
-// }
-
 "use client"
 import React, { useEffect, useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -85,7 +26,7 @@ export default function ProjectSwiper() {
 
 
   useEffect(() => {
-    fetch('/data/ProjectsData.json')
+    fetch('/data/projects.json')
       .then((res) => res.json())
       .then((data) => {
         console.log('Fetched data:', data);
