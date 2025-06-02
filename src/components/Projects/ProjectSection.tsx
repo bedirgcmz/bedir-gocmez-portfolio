bb2571c4cc4fb091d0e75bf8e9b79f7b3fe4810f
bedirgcmz <bdrgcmz@gmail.com>
2025-06-02 08:41:19 +0200

    Added controls from motions for eslint

--- src/components/Projects/ProjectSection.tsx
+++ src/components/Projects/ProjectSection.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ProudOfProjects from './ProudOfProjects/ProudOfProject'
 import SchoolProjects from './SchoolProject/SchoolProjects'
+import { useAnimation } from 'framer-motion';
+
 
 const ProjectSection = () => {
+  const controls = useAnimation();
   return (
     <section>
         <ProudOfProjects />
-        <SchoolProjects controls={undefined} />
+        <SchoolProjects controls={controls} />
     </section>
   )
 }
